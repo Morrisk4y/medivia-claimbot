c275fbf111d43e22c472a841f065a5ac841d80f2
morris kellokoski <morriskellokoski@gmail.com>
2024-05-22 21:28:13 +0200

    changed alot of the code since i bumped into discord limitations

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-CLAIM_DURATION_SECONDS = int(os.getenv("CLAIM_DURATION_SECONDS", 20))
-RESUME_DURATION_SECONDS = int(os.getenv("RESUME_DURATION_SECONDS", 15))
-ACCEPT_CLAIM_SECONDS = int(os.getenv("ACCEPT_CLAIM_SECONDS", 10))
+CLAIM_DURATION_SECONDS = int(os.getenv("CLAIM_DURATION_SECONDS", 3600))
+RESUME_DURATION_SECONDS = int(os.getenv("RESUME_DURATION_SECONDS", 900))
+ACCEPT_CLAIM_SECONDS = int(os.getenv("ACCEPT_CLAIM_SECONDS", 900))
 GUILD_ID = int(os.getenv("GUILD_ID"))
 PREFIX = os.getenv('PREFIX')
 intents = discord.Intents.all()
@@ -62,6 +62,20 @@ async def next(ctx):
     else:
         await ctx.respond("No one has claimed the spot. Type /claim to claim the spot.", ephemeral=True)
 
+@bot.slash_command(name="timer", description="Get the current timer for the hunting spot")
+async def timer(ctx):
+    channel_id = ctx.channel.id
+    if channel_id in timers and not timers[channel_id]['task'].done():
+        duration = timers[channel_id]['task']._coro.cr_frame.f_locals['duration']
+        
+        hours, remainder = divmod(duration, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        time_display = f"{hours:02}:{minutes:02}:{seconds:02}"
+        
+        await ctx.respond(f"Time remaining for the current claim: {time_display}", ephemeral=True)
+    else:
+        await ctx.respond("There is no active timer for this channel.", ephemeral=True)
+
 
 @bot.slash_command(name="listqueue", description="List all users currently in the queue for the hunting spot")
 async def listqueue(ctx):
@@ -112,7 +126,7 @@ async def resume(ctx, image: Option(discord.Attachment, description="Upload an i
     if channel_id in resume_timers and not resume_timers[channel_id]['task'].done():
         if current_owner.get(channel_id) == user_id:
             resume_timers[channel_id]['task'].cancel()  # Cancel existing timer
-            await ctx.respond("Your claim timer has been resumed.", ephemeral=True)
+            await ctx.respond("Your claim timer has been resumed", ephemeral=True)
             initiate_claim_sequence(ctx, channel_id, user_id, resume_timers[channel_id]['message'])
             del resume_timers[channel_id]
         else:
@@ -126,64 +140,92 @@ async def leave(ctx):
     channel_id = ctx.channel.id
     user_id = ctx.author.id
     
-    # If the user is the current owner and has an active timer
     if channel_id in current_owner and current_owner[channel_id] == user_id:
-        # Cancel the active timer
         if channel_id in timers and timers[channel_id]['task']:
-            timers[channel_id]['task'].cancel()
-            await update_embed(timers[channel_id]['message'], "Hunting Spot Available", "player left the spawn. The spot is now available for claiming. if no one is in the queue", discord.Color.green(), None)
+            timers[channel_id]['task'].cancel()  # Cancel the active timer
+            old_message = timers[channel_id]['message']
+            new_message = await create_new_embed(ctx, "Hunting Spot Available", "Player left the spawn. The spot is now available for claiming. If no one is in the queue", discord.Color.green())
+            if isinstance(old_message, discord.Message):
+                await old_message.delete()  # Delete the old embed
+            timers[channel_id]['message'] = new_message  # Update the message reference
             del current_owner[channel_id]
-        
-        # Check the next person in the queue
+
         if queues[channel_id]:
             next_user_id = queues[channel_id].pop(0)
             next_user = await ctx.guild.fetch_member(next_user_id)
-            current_owner[channel_id] = next_user_id  # Set the next user as the current owner
+            current_owner[channel_id] = next_user_id
             await ctx.respond(f"{ctx.author.display_name} has left. {next_user.mention}, it's your turn to accept the claim.")
-            # Start the accept claim timer for the next user
             timers[channel_id] = {
-                'task': asyncio.create_task(accept_claim_timer(ctx, timers[channel_id]['message'], next_user_id)),
-                'message': timers[channel_id]['message']
+                'task': asyncio.create_task(accept_claim_timer(ctx, new_message, next_user_id)),
+                'message': new_message
             }
         else:
             await ctx.respond("You have left the queue and no one else is in line.", ephemeral=True)
             if channel_id in timers:
-                del timers[channel_id]  # Clear any associated timers if no one is in the queue
+                del timers[channel_id]
     else:
-        # If the user is not the current owner but in the queue
-        if user_id in queues[channel_id]:
+        if queues[channel_id] and user_id in queues[channel_id]:
             queues[channel_id].remove(user_id)
             await ctx.respond(f"{ctx.author.display_name}, you have been removed from the queue.", ephemeral=True)
         else:
             await ctx.respond("You are not currently owning or in the queue.", ephemeral=True)
-            
+
+
+async def create_new_embed(ctx, title, description, color, owner=None):
+    embed = discord.Embed(title=title, description=description, color=color)
+    embed.set_footer(text=f"Claimed by: {ctx.author.display_name}")
+    return await ctx.send(embed=embed)
+  
 def initiate_claim_sequence(ctx, channel_id, user_id, message):
+    print(f"Initiating claim sequence: channel_id={channel_id}, user_id={user_id}")
     current_owner[channel_id] = user_id
-    # Ensure only one timer is set at a time, handling possible duplicate timer starts
+
     if 'task' in timers.get(channel_id, {}):
+        print(f"Cancelling existing timer for channel_id={channel_id}")
         timers[channel_id]['task'].cancel()
+
     timers[channel_id] = {
         'task': asyncio.create_task(handle_timer(ctx, message, CLAIM_DURATION_SECONDS, user_id)),
         'message': message
     }
+
     if channel_id not in queues:
         queues[channel_id] = []
 
+    print(f"Claim sequence initiated: current_owner={current_owner[channel_id]}, timers={timers[channel_id]}")
+
 async def handle_timer(ctx, message, duration, owner):
     channel_id = ctx.channel.id
-    while duration > 0:
-        await asyncio.sleep(60)
-        duration -= 60
-        remaining_time = max(duration, 0)  # Ensure the remaining time doesn't go negative
+    print(f"Starting timer for channel_id={channel_id}, owner={owner}, duration={duration} seconds")
+    try:
+        previous_message = message
+        while duration > 0:
+            await asyncio.sleep(30)
+            duration -= 30
+            remaining_time = max(duration, 0)  # Ensure the remaining time doesn't go negative
 
-        # Calculate hours, minutes, and seconds
-        hours, remainder = divmod(remaining_time, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        time_display = f"{hours:02}:{minutes:02}:{seconds:02}"
+            # Calculate hours, minutes, and seconds
+            hours, remainder = divmod(remaining_time, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            time_display = f"{hours:02}:{minutes:02}:{seconds:02}"
 
-        await update_embed(message, "Hunting Spot Timer", f"Time Remaining: {time_display}", discord.Color.blue(), owner)
-    
-    await start_resume_timer(ctx, message, owner)
+            print(f"Timer update for channel_id={channel_id}: {time_display} remaining")
+
+            try:
+                # Create a new embed message
+                new_message = await create_new_embed(ctx, "Hunting Spot Timer", f"Time Remaining: {time_display}", discord.Color.blue(), owner)
+                if isinstance(previous_message, discord.Message):
+                    await previous_message.delete()
+                previous_message = new_message
+            except Exception as e:
+                print(f"Exception occurred while creating new embed for channel_id={channel_id}: {e}")
+        
+        print(f"Timer ended for channel_id={channel_id}, starting resume timer")
+        await start_resume_timer(ctx, previous_message, owner)
+    except asyncio.CancelledError:
+        print(f"Timer task for channel_id={channel_id} was cancelled.")
+    except Exception as e:
+        print(f"Unexpected exception in timer for channel_id={channel_id}: {e}")
 
 async def start_resume_timer(ctx, message, owner):
     channel_id = ctx.channel.id
@@ -235,18 +277,24 @@ async def accept_claim_timer(ctx, message, next_user_id):
 
 async def accept_claim_countdown(ctx, message, duration, next_user_id, next_user_display_name):
     channel_id = ctx.channel.id
-    while duration > 0:
-        await asyncio.sleep(10)
-        duration -= 10
-        await update_embed(message, "Claim Timer", f"{next_user_display_name}, you have {duration} seconds to /acceptclaim.", discord.Color.orange(), next_user_id)
+    try:
+        while duration > 0:
+            await asyncio.sleep(10)
+            duration -= 10
 
-    if duration == 0 and current_owner[channel_id] == next_user_id:
-        await update_embed(message, "Claim Timer Expired", f"{next_user_display_name} did not claim in time. The next person can claim.", discord.Color.red(), None)
-        if queues[channel_id]:
-            await process_queue(ctx, message, channel_id)
-        else:
-            del current_owner[channel_id]  # Clean up if no response
-        del acceptclaim_timers[channel_id]
+            new_message = await create_new_embed(ctx, "Claim Timer", f"{next_user_display_name}, you have {duration} seconds to /acceptclaim.", discord.Color.orange(), next_user_id)
+            if isinstance(message, discord.Message):
+                await message.delete()
+            message = new_message
+            acceptclaim_timers[channel_id]['message'] = message
+
+        # Handle the scenario when the timer expires
+        # You may need to add additional logic here
+    except asyncio.CancelledError:
+        print(f"Accept claim timer for channel_id={channel_id} was cancelled.")
+    except Exception as e:
+        print(f"Unexpected exception in accept claim timer for channel_id={channel_id}: {e}")
+
 
         
 async def process_queue(ctx, message, channel_id):
@@ -269,29 +317,25 @@ async def accept_claim_timer(ctx, message, next_user_id):
     channel_id = ctx.channel.id
     duration = ACCEPT_CLAIM_SECONDS
     try:
-        # Ensure next_user_id is a numeric ID and fetch the member
-        next_user_id = int(next_user_id)  # Converts to integer if it's a string number, fails if it's not a valid number
+        next_user_id = int(next_user_id)
         next_user = await ctx.guild.fetch_member(next_user_id)
         next_user_display_name = next_user.display_name if next_user else "Unknown User"
 
-        await ctx.send(f"{next_user.mention}, you have {duration} seconds to /acceptclaim.")  # Proper mention
-        await update_embed(message, "Claim Timer", f"{next_user_display_name}, you have {duration} seconds to /acceptclaim.", discord.Color.orange(), next_user_id)
+        await ctx.send(f"{next_user.mention}, you have {duration} seconds to /acceptclaim.")
 
-        while duration > 0:
-            await asyncio.sleep(10)
-            duration -= 10
-            await update_embed(message, "Claim Timer", f"{next_user_display_name}, you have {duration} seconds to /acceptclaim.", discord.Color.orange(), next_user_id)
+        # Create new embed and delete the old one
+        new_message = await create_new_embed(ctx, "Claim Timer", f"{next_user_display_name}, you have {duration} seconds to /acceptclaim.", discord.Color.orange(), next_user_id)
+        if isinstance(message, discord.Message):
+            await message.delete()
+        message = new_message
 
-        if duration == 0 and current_owner[channel_id] == next_user_id:
-            await update_embed(message, "Claim Timer Expired", f"{next_user_display_name} did not claim in time. The next person can claim.", discord.Color.red(), None)
-            if queues[channel_id]:
-                await process_queue(ctx, message, channel_id)
-            else:
-                del current_owner[channel_id]  # Clean up if no response
-    except ValueError:
-        await ctx.send("Invalid user ID. User IDs must be numeric.")
-    except discord.NotFound:
-        await ctx.send("User not found. Please ensure the user ID is correct.")
+        acceptclaim_timers[channel_id] = {
+            'task': asyncio.create_task(accept_claim_countdown(ctx, message, duration, next_user_id, next_user_display_name)),
+            'message': message
+        }
+    except Exception as e:
+        print(f"Exception occurred while starting accept claim timer for channel_id={channel_id}: {e}")
+
 
 
 
@@ -302,6 +346,7 @@ async def update_embed(message, title, description, color, owner_id=None):
         owner_display_name = owner.display_name if owner else "Unknown Owner"
         embed.set_footer(text=f"Claim owned by: {owner_display_name}")
     await message.edit(embed=embed)
+    
 
 @bot.event
 async def on_ready():
